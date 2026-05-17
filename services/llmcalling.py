42f594e009fb187d6f18d2827e876f54e195a056
harthik <harthikwork@gmail.com>
2026-05-17 15:15:48 +0530

    change to adaptive interviewer by adding eval metrics

--- services/llmcalling.py
+++ services/llmcalling.py
@@ -2,7 +2,7 @@ from google import genai
 import os
 from dotenv import load_dotenv
 load_dotenv()
-def llmcalling(context,user,history):
+def llmcalling(context,user,history,evaluation_state):
     
     # The client gets the API key from the environment variable `GEMINI_API_KEY`.
     client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
@@ -10,12 +10,24 @@ def llmcalling(context,user,history):
     response = client.models.generate_content(
         model="gemini-2.5-flash", contents=
         f"""
-        You are a professional AI interviewer.
+        You are a adaptive AI interviewer.
 
-        Your job:
+        your goals are to evaluate the candidate:
+        1. technical depth
+        2. communication
+        3. problem solving
+        4. project experience
+
+        rules:
         - Ask one interview question at a time.
-        - give feedback on users previous response.
-        - give a score out of 10.
+        - avoid repeating topics that are already covered.
+        - Track which skills have enough evidence.
+        - If enough evidence exists across all evaluation areas, stop the interview.
+        - Stop when additional questions are unlikely to improve evaluation quality.
+        - Prefer targeted follow-up questions for weak areas.
+
+        Interview Evaluation State:
+        {evaluation_state}
 
         Candidate Resume:
         {context}
@@ -26,20 +38,27 @@ def llmcalling(context,user,history):
         Latest user answer:
         {user}
 
-        Instructions:
-        - If no history, ask an introduction question and set feedback="" and score=0
-        - Otherwise:
-            - Give feedback on the user's answer
-            - Give a score from 1 to 10
-            - Ask the next relevant question
-        - Do NOT repeat questions
+        If this is first interaction:
+        - start with the basic introduction and ask the candidate to briefly introduce themselves and their background.
+        - set score to 0
+        - decision = "continue"
+
+        Do not stop the interview before at least 5 questions have been asked.
 
 
         respond strictly in JSON format:
         {{
-            "question:"next question here",
+            "decision":"continue" or "stop",
+            "question":"next question here",
             "feedback":"feedback here",
-            "score":number
+            "score":number,
+            "evaluation_update":{{
+                "technical_depth":number,
+                "communication":number,
+                "problem_solving":number,
+                "project_experience":number
+            }},
+            "reason":"why continue or stop"
         }}
 
         do not add anything else.
